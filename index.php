55332d5d5b2c344b6467692967c16eb2111dd382
seven <jheancg@gmail.com>
2016-02-15 11:55:27 -0430

    Primer commit de la pagina C&F engineering

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<!-- <meta charset="iso-8859-1"> -->
+	<meta charset="utf-8" />
+	<meta name="description" content="Ejemplo de HTML5" />
+	<meta name="keywords" content="HTML5, CSS3, Javascript" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>C&F</title>
+	<link rel="stylesheet" type="text/css" href="css/stylehome.css">
+	<link rel="stylesheet" type="text/css" href="css/camera.css">
+	<script type='text/javascript' src='js/jquery.min.js'></script>
+	<script type='text/javascript' src='js/jquery.mobile.customized.min.js'></script>
+	<script type='text/javascript' src='js/jquery.easing.1.3.js'></script> 
+	<script type='text/javascript' src='js/camera.min.js'></script>
+	<script>
+		jQuery(function(){
+			
+			jQuery('#camera_wrap_4').camera({
+				height: 'auto',
+				loader: 'pie',
+				time: 2500,
+				transPeriod: 750,
+				navigation: false,
+				pagination: false,
+				playPause: false,
+				pauseOnClick: false,
+				thumbnails: false,
+				hover: false,
+				opacityOnGrid: false,
+				imagePath: '../images/'
+			});
+
+		});
+	</script>
+</head>
+<body>
+	<div class="content">
+	<div id="back_to_camera">
+		<a href="http://www.pixedelic.com/plugins/camera/">&larr; Back to the Camera project</a>
+	</div>
+	<!-- #back_to_camera -->
+
+	<div class="fluid_container">
+		<div class="camera_wrap camera_emboss pattern_1" id="camera_wrap_4">
+			<div data-thumb="images/slides/thumbs/bridge.jpg" data-src="images/slides/refineria1.jpg"></div>
+			<div data-thumb="images/slides/thumbs/leaf.jpg" data-src="images/slides/refineria2.jpg"></div>
+			<div data-thumb="images/slides/thumbs/road.jpg" data-src="images/slides/refineria3.jpg"></div>
+			<div data-thumb="images/slides/thumbs/sea.jpg" data-src="images/slides/refineria4.jpg"></div>
+			<div data-thumb="images/slides/thumbs/shelter.jpg" data-src="images/slides/refineria5.jpg"></div>
+		</div>
+		<!-- #camera_wrap -->
+	</div>
+	<br clear="all">
+	<!-- .fluid_container -->
+	</div>
+	<footer>
+		Derechos Reservados &copy; 2014
+	</footer>
+</body>
+</html>
