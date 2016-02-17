2587de715487ba444ff8bcc09dd3e6a14ce98f5e
seven <jheancg@gmail.com>
2016-02-16 20:01:35 -0430

    se agrego el favicon y se mejoro la imagen que faltaba del slider

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
 	<!-- <meta charset="iso-8859-1"> -->
 	<meta charset="utf-8" />
-	<meta name="description" content="Ejemplo de HTML5" />
+	<meta name="Title" content="C&F Engineering">
+    <meta name="description" content="texto unico de cada pagina 70 156 caracteres">
 	<meta name="keywords" content="HTML5, CSS3, Javascript" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>C&F</title>
+	<title>C&amp;F Engineering</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="css/stylehome.css">
 	<link rel="stylesheet" type="text/css" href="css/camera.css">
 	<script type='text/javascript' src='js/jquery.min.js'></script>
@@ -45,7 +47,7 @@
 	<div class="fluid_container">
 		<div class="camera_wrap camera_emboss pattern_1" id="camera_wrap_4">
 			<div data-thumb="images/slides/thumbs/bridge.jpg" data-src="images/slides/refineria1.jpg"></div>
-			<div data-thumb="images/slides/thumbs/leaf.jpg" data-src="images/slides/refineria2.jpg"></div>
+            <div data-thumb="images/slides/thumbs/leaf.jpg" data-src="images/slides/refineria2.jpg"></div>
 			<div data-thumb="images/slides/thumbs/road.jpg" data-src="images/slides/refineria3.jpg"></div>
 			<div data-thumb="images/slides/thumbs/sea.jpg" data-src="images/slides/refineria4.jpg"></div>
 			<div data-thumb="images/slides/thumbs/shelter.jpg" data-src="images/slides/refineria5.jpg"></div>
