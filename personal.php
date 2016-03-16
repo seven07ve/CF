6c95eebe060719205c1ddc7fd9439f3acce0a43d
seven <jheancg@gmail.com>
2016-03-15 20:58:34 -0430

    se hizo la seccion de experiencia de nuestro personal hay que hacer la de hecho en casa

---
+++ personal.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- <meta charset="iso-8859-1"> -->
+    <meta charset="utf-8" />
+    <meta name="Title" content="C&F Engineering">
+    <meta name="description" content="Empresa de Consultoría especializada en la creación de Soluciones integrales para la Industria Petrolera, Gasífera, Petroquímica, Energética y de Servicios, con prestación de asistencia técnica durante las etapas de diseño de Proyecto y Arranque de instalaciones. ">
+    <meta name="keywords" content="Empresa, Consultoría, creación, Soluciones, Soluciones integrales, Industria, Petrolero, Gasífero, Petroquímico, Energético, Servicios, asistencia técnica,  diseño, proyecto, Arranque, Arranque de instalaciones, instalaciones" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SERVICIOS C&amp;F Engineering</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <!--	fuente RALEWAY-->
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,800' rel='stylesheet' type='text/css'>
+        <!--fuente BITTER-->
+    <link href='https://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/camera.css">
+        <!--    PAGE SCROLL-->
+    <link rel="stylesheet" href="css/pagescroll.css">
+        <!-- Bootstrap core CSS -->
+    <link href="./bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="./bootstrap/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./bootstrap/navbar.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="./bootstrap/ie-emulation-modes-warning.js"></script>
+    <script type='text/javascript' src='js/jquery.min.js'></script>
+    <script type='text/javascript' src='js/jquery.mobile.customized.min.js'></script>
+    <script type='text/javascript' src='js/jquery.easing.1.3.js'></script>
+</head>
+<body>
+
+
+       <?php $experiencia=active; include("php/menu.php"); ?>
+        <section class="cuadros-conten bg-1">
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Coordinación de equipos Multidisciplinarios Gerencia de Ingeniería y Proyectos PDVSA Oriente para IC, IB e ID Proyectos Propanoducto, Endulzamiento de Gas Fase I y Fase II, Endulzamiento de Gas en Planta de COA. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Petrobras <br>
+				<strong>REFERENCIA</strong> Asesoría en Fase FEL Proyecto Plan Director de Refino. <br>
+				<strong>LUGAR:</strong> Argentina.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Pacific Rubiales Energy (PRE) <br>
+				<strong>REFERENCIA</strong> Dirección de Proyectos para para ID Plataformas de Inyección (PAD) 3 y 8.  <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Ecopetrol <br>
+				<strong>REFERENCIA</strong> Gerencia de Ingeniería para EPC 1, 2 y 3 Plan Maestro de Servicios Industriales de Refinería Barrancabermeja”. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Control y Estimación de Costos para Proyecto Modificación Unidad de Alquilación de la Refinería PLC; Proyecto RFCC de la Refinería El Palito; Ceras Parafínicas – PDVSA; GLP El Palito; Infraestructura USI-PQV; Muelle USI-PQV. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Sincor <br>
+				<strong>REFERENCIA</strong> Gerencia Ingeniería Básica y de Detalle Instalación Tanque de 750 MBls de Crudo. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> OBC <br>
+				<strong>REFERENCIA</strong> Dirección de Proyectos para para IB Oleoducto Bicentenario Tramo Banadía-Ayacucho-Coveñas. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Empresa RYPF <br>
+				<strong>REFERENCIA</strong> Gerencia de Ingeniería para el IPC de Recuperación Gases de Antorcha en el Complejo Industrial de la Plata. <br>
+				<strong>LUGAR:</strong> Argentina.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Sincor <br>
+				<strong>REFERENCIA</strong> Gerencia de Ingeniería Básica y de Detalle de 25 Proyectos Operacionales y de Mantenimiento para la Operadora Sincrudos de Oriente (Sincor). <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> OCN <br>
+				<strong>REFERENCIA</strong> Gerencia Ingeniería Básica y de Detalle Nuevo Tren de Tratamiento de Agua en el Complejo Mejorador OCN. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> OCN <br>
+				<strong>REFERENCIA</strong> Gerencia Ingeniería Básica y de Detalle de Proyectos Operacionales para la Operadora Cerro Negro (Exxon Mobil/PDVSA). <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>REFERENCIA</strong> Formación de Líderes y Coaching Gerencial. <br>
+			</div>
+			<div class="cuadros">
+				<strong>REFERENCIA</strong> Cursos de Gerencia de Proyectos de Inversión. Talleres de Ingeniería de Valor. <br>
+			</div>
+			<!--segunda-->
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Comisión Federal de Electricidad (CFE) <br>
+				<strong>PROYECTO</strong> EPC “222 CC Repotenciación Central Termoeléctrica Manzanillo I, U-1 y 2”. <br>
+				<strong>LUGAR:</strong> México.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Metanol de Oriente Complejo Industrial José Antonio Anzoátegui <br>
+				<strong>PROYECTO</strong> EPC “METOR Expansion”. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Pemex <br>
+				<strong>PROYECTO</strong> IB e ID “Mejoramiento de las Plantas de Recuperación de Azufre I, II y III en la Refinería Ing. Antonio Dovali Jaime en Salina Cruz, Oaxaca”. <br>
+				<strong>LUGAR:</strong> México.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Repsol <br>
+				<strong>PROYECTO</strong> IB e ID “Planta de Extracción de líquidos Quiamare - La Ceiba (REPSOL YPF)”.  <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Ecopetrol <br>
+				<strong>PROYECTO</strong> EPC 1, 2 y 3 Plan Maestro de Servicios Industriales de Refinería Barrancabermeja”. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Ameriven <br>
+				<strong>PROYECTO</strong> ID Proyectos CAPEX de AMERIVEN: “New H2S Removal Facilities Project”; “Educt and vent piping Project”; “Facilidades para la segregación del slop liviano craqueado”. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Pemex <br>
+				<strong>PROYECTO</strong> IB e ID “Mejoramiento de la Unidad de Tratamiento con DEA de FCC2 de la Refinería de Cadereyta en Nuevo León”. <br>
+				<strong>LUGAR:</strong> México.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Petrozuata <br>
+				<strong>PROYECTO</strong> IC “Alternativas de Reparación de Techos de Tanques T-202A/B. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> OBC <br>
+				<strong>PROYECTO</strong> IB “Oleoducto Bicentenario Tramo Banadía-Ayacucho-Coveñas”. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Ocensa <br>
+				<strong>PROYECTO</strong> ID “Estación El Porvenir para el incremento de capacidad de transporte de crudo pesado hasta 300 cSt”. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> CBI <br>
+				<strong>PROYECTO</strong> ID Proyecto de Expansión de la Refinería de Cartagena – CBI - Reficar. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Repsol <br>
+				<strong>PROYECTO</strong> IC “Reactivación Campo Quiriquire Shallow”  / “Reducción de Presión de Separación Estación de Flujo Tácata” / “Mudanza estación de flujo Batería 12 Guárico” /. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Oleoducto de los Llanos S.A (ODL) <br>
+				<strong>PROYECTO</strong> IB “Oleoducto Carmentea-Araguaney”. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Repsol <br>
+				<strong>PROYECTO</strong> IB e ID “Adecuaciones de instalaciones de superficie Campo Quiamare-La Ceiba”. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Repsol <br>
+				<strong>PROYECTO</strong> IB e ID “Sistema de manejo Agua de formación Campo Quiamare- La Ceiba”. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Repsol <br>
+				<strong>PROYECTO</strong> Selección esquema de compresión Planta QE-2  / Estudio de Factibilidad Planta de Control de Rocío campo Quiriquire. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<!--tercera-->
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Ameriven <br>
+				<strong>REFERENCIA</strong> Análisis de Flexibilidad líneas asociadas a Fired Heater F-1102. Cerro Negro Upgrader Project. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Petrobras <br>
+				<strong>REFERENCIA</strong> Análisis de Flexibilidad y Vibraciones de líneas asociadas a la EPF Pata Azul/PATA, Estación de Producción ubicada en el Bloque 18. <br>
+				<strong>LUGAR:</strong> Ecuador.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Repsol <br>
+				<strong>REFERENCIA</strong> Estudio de flexibilidad tubería de producción Pozo QQ-675. Proyecto Adecuación de Puentes de Tuberías en Río Azaguas. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> OCN <br>
+				<strong>REFERENCIA</strong> Análisis de Flexibilidad Bombas asociadas al Sistema Diluente OCN-Jose. Diluent System Upgrades Project.  <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis de Flexibilidad Proyecto Ampliación de la Capacidad de Deshidratación de Crudo del Complejo Jusepín EP-JUS-2. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis de flexibilidad sistemas de tuberías asociados al área de separadores a instalar. Proyecto de reordenamiento Planta Compresora Muscar. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis de Flexibilidad Patio de Tanques Oficina - Incremento Capacidad de almacenamiento en PTO y manejo de producción pesados por tubería de 42”. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> OCN <br>
+				<strong>REFERENCIA</strong> Análisis de flexibilidad Sistema de Tubería asociado al Proyecto de ampliación de flexibilidad operacional del tanque 53-Tk-5309. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Williams International <br>
+				<strong>REFERENCIA</strong> Determinación de cargas sobre las boquillas del turboexpansor 12-0601, Planta ACCROVEN San Joaquín. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> CBI &amp; Lummus <br>
+				<strong>REFERENCIA</strong> Análisis de Flexibilidad IB e ID en CB&amp;I – Lummus. <br>
+				<strong>LUGAR:</strong> USA.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Empresas Públicas de Medellín E.P.M <br>
+				<strong>REFERENCIA</strong> Estudio de análisis de flexibilidad y soportería de tuberías del sistema de generación de vapor de la Central Termoeléctrica La Sierra. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis de Flexibilidad Proyecto Oportunidades de Gas-San Tomé recolección de gas baja presión pesado oeste – área Melones. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<!--cuarta-->
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis por Golpe de Ariete de las 6 troncales de alimentación de la Refinería Puerto La Cruz. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis por Golpe de Ariete Diluenducto Complejo Criogénico José Antonio Anzóátegui – Patio de Tanques Oficina, PDVSA Oriente. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Oleoducto de los Llanos (ODL) <br>
+				<strong>REFERENCIA</strong> Análisis por Golpe de Ariete Oleoducto Carmentea-Araguaney. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis por Golpe de Ariete del sistema para manejo de Gasolina y Nafta de la Refinería Puerto la Cruz.  <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Comisión Federal de Electricidad (CFE) <br>
+				<strong>REFERENCIA</strong> Análisis “Steam Hammer” Central Termoeléctrica Manzanillo I, U1 y U2, Colima México. <br>
+				<strong>LUGAR:</strong> México.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> OBC <br>
+				<strong>REFERENCIA</strong> Análisis por Golpe de Ariete Oleoducto Bicentenario Tramo Banadía-Ayacucho-Coveñas, Proyecto Bicentenario. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis por Golpe de Ariete 6 Muelles del Terminal Marino Guaraguao- PDVSA Oriente. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> Pacific Rubiales Energy (PRE) <br>
+				<strong>REFERENCIA</strong> Análisis por Golpe de Ariete Plataformas Múltiples de Inyección (PAD) 1,3,5,6,7 y 9. <br>
+				<strong>LUGAR:</strong> Colombia.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis por Golpe de Ariete 4 Muelles del Terminal Marino El Chaure- PDVSA Oriente. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<div class="cuadros">
+				<strong>CLIENTE:</strong> PDVSA <br>
+				<strong>REFERENCIA</strong> Análisis por Golpe de Ariete, Instalaciones Complementarias BITOR, PDVSA Oriente. <br>
+				<strong>LUGAR:</strong> Venezuela.
+			</div>
+			<br clear="all">
+        </section>
+        <section class="section section-9 hidden-xs visible-sm visible-md visible-lg"> <?php include("php/footer.php"); ?>
+		</section>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!--<script src="./bootstrap/jquery.min.js"></script>-->
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="./bootstrap/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="./bootstrap/ie10-viewport-bug-workaround.js"></script>
+    <!--SCROLL SCRIPT-->
+    <script src="js/jquery-1.11.3.min.js"></script>
+</body>
+</html>
---
+++ prueba.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title></title>
+				    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+					<!--	fuente RALEWAY-->
+					<link href='https://fonts.googleapis.com/css?family=Raleway:400,800' rel='stylesheet' type='text/css'>
+					<!--fuente BITTER-->
+					<link href='https://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+					<link rel="stylesheet" type="text/css" href="css/style.css">
+					<link rel="stylesheet" type="text/css" href="css/camera.css">
+					<!--    PAGE SCROLL-->
+					<link rel="stylesheet" href="css/pagescroll.css">
+					<!-- Bootstrap core CSS -->
+					<link href="./bootstrap/bootstrap.min.css" rel="stylesheet">
+
+					<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+					<link href="./bootstrap/ie10-viewport-bug-workaround.css" rel="stylesheet">
+<style type="text/css">
+.cuadros{
+	font-size: 8px;
+	padding: 2em;
+	width:150px; height: 150px; float: left; display:inline-block; margin: 2%; border-radius: 0px 20px 0px 20px;
+	background: #FFF;
+	-webkit-transition: 0.5s;
+    transition: 500ms 0ms;
+	border-top: 2px solid #5f8ebc;
+	border-left: 2px solid #5f8ebc;
+	border-bottom: 2px solid #787979;
+	border-right: 2px solid #5f8ebc;
+	overflow: hidden;
+}
+.cuadros:hover {
+	font-size: 12px;
+    width: 320px;
+	height: 320px;
+	border-radius: 20px;
+}
+.cuadros-conten{
+		width:100%; height: auto; padding:2%;
+	}
+</style>
+</head>
+<body>
+			<div class="cuadros-conten">
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<div class="cuadros">
+					<strong>CLIENTE:</strong> PDVSA <br>
+					<strong>REFERENCIA</strong> Gerencia de Ingeniería y Proyectos PDVSA Oriente, Coordinación, Seguimiento y Control de Proyectos en Etapa FEL: Instalaciones Complementarias BITOR; Planta de Manufactura de Orimulsión; Remoción de H2S y CO2 en el Gas Natural; Ampliación Criogénico de Oriente III y IV; Exportación de Etano ACCRO III y IV; Manejo de Crudo Nueva Planta Compresora Altagracia de Orituco; Proyecto Interconexión Centro Oriente ICO <br>
+					<strong>LUGAR:</strong> Venezuela.
+				</div>
+				<br clear="all">
+			</div>
+</body>
+</html>
