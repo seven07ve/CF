90438a5f714ad14b09260cf5bbcbc601442243b3
seven <jheancg@gmail.com>
2016-03-28 22:28:16 -0430

    el margen derecho de bootstrap se llevo a cero para solucionar el peo de siempre del pedaso que sobresale se llevo de col-11 a col-12 para que quepa comodo todo el texto de los directores

--- experiencia.php
+++ experiencia.php
@@ -43,7 +43,7 @@
             <div class="col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-offset-2 col-lg-8">
                 <h3 style="margin-bottom:2%;">DIRECTORES C&amp;F ENG - DIRECTORA EJECUTIVA</h3>
                 <div class="row">
-                    <div class="col-xs-11 col-sm-11 col-md-offset-1 col-md-11 text-justify text-mpeq">
+                    <div class="col-xs-11 col-sm-11 col-md-12 text-justify text-mpeq">
                         <img class="img-responsive pull-right" src="imagenes/natalia.jpg" style="width:25%; margin-left:4%; border-radius:8px; border: 1px solid #A0A0A0; box-shadow: #555 4px 4px 8px;" alt="Ivan Gamero" title="Ivan Gamero">
                         <h4>Natalia Cataldo</h4>
                         Gerente Senior de Proyectos, con más de 30 años de experiencia y exitoso
@@ -101,7 +101,7 @@
                 <h3>DIRECTORES C&amp;F ENG - DIRECTOR DE COMISIONAMIENTO Y ARRANQUE
 </h3>
                 <div class="row">
-                    <div class="col-xs-11 col-sm-11 col-md-offset-1 col-md-11 text-justify text-mpeq">
+                    <div class="col-xs-11 col-sm-11 col-md-12 text-justify text-mpeq">
                         <img class="img-responsive pull-right" src="imagenes/ivan.jpg" style="width:25%; margin-left:4%; border-radius:8px; border: 1px solid #A0A0A0; box-shadow: #555 4px 4px 8px;" alt="Ivan Gamero" title="Ivan Gamero">
                         <h4>Ivan Gamero</h4>
                         Gerente de Commissioning para Petróleo y Gas Natural. 33 años de experiencia.
@@ -143,7 +143,7 @@
             <div class="col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-offset-2 col-lg-8">
                 <h3>DIRECTORES C&amp;F ENG - DIRECTOR TÉCNICO</h3>
                 <div class="row">
-                    <div class="col-xs-11 col-sm-11 col-md-offset-1 col-md-11 text-justify text-mpeq">
+                    <div class="col-xs-11 col-sm-11 col-md-12 text-justify text-mpeq">
                         <img class="img-responsive pull-right" src="imagenes/mariby.jpg" style="width:25%; margin-left:4%; border-radius:8px; border: 1px solid #A0A0A0; box-shadow: #555 4px 4px 8px;" alt="Ivan Gamero" title="Ivan Gamero">
                         <h4>Mariby Figuera</h4>
                         Ingeniero Mecánico e Ingeniero de Sistemas Mención Control de Procesos con más
